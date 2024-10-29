a25e9368852117bbc5d6467b9d7d5f290aaab17e
VyacheslavvF <brikker.va@att.edu.ru>
2024-10-29 17:15:53 +0300

    realization of the card

--- src/main/java/bank/Card.java
+++ src/main/java/bank/Card.java
@@ -1,4 +1,23 @@
 package bank;
 
 public class Card {
+    private final int CARD_NUMBER;
+    private int pinCode;
+
+    public Card(int CARD_NUMBER, int pinCode) {
+        this.pinCode = pinCode;
+        this.CARD_NUMBER = CARD_NUMBER;
+    }
+
+    public int getCARD_NUMBER() {
+        return CARD_NUMBER;
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(int pinCode) {
+        this.pinCode = pinCode;
+    }
 }
